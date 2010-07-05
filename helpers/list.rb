72d9c03f2cc56aa8f7664ea87924ff67144dd020
Henrik N <henrik@nyh.se>
2010-07-05 21:56:43 +0200

    Item dates. List page styling.

--- helpers/list.rb
+++ helpers/list.rb
@@ -23,5 +23,33 @@ helpers do
   def format_description(description)
     DescriptionFormatter.new(description).format
   end
+  
+  def date_attributes(item)
+    days_ago = (Date.today - item.added).to_i
+    klass = days_ago < 7 ? "days-ago-#{days_ago}" : nil
+    {
+      :title => "Added #{item.added}",
+      :class => klass
+    }
+  end
+
+  def humanize_date(date)
+    now = Time.now.utc
+    today = Date.new(now.year, now.month, now.day)
+
+    format_string =
+      if date == today
+        "Today"
+      elsif date == today - 1  # Yesterday.
+        "Yesterday"
+      elsif date > today - 7  # This week.
+        "%A"  # e.g. "Monday"
+      else
+        year = date.year < Date.today.year ? ", %Y" : ""
+        date_format = "%b %d#{year}"  # e.g. "Jul 5, 2010"
+      end
+    date.strftime(format_string).
+      sub(/\b0/, "")  # Remove leading zeros.
+  end
 
 end
