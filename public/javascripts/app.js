d71bd848d077e0dc47d6a98679f869029c200af6
Henrik N <henrik@nyh.se>
2010-07-06 21:28:33 +0200

    Relativize note dates.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,8 +3,54 @@ List = (function() {
     init: function() {
       initFancyDrilldowns();
       mendAnchors();
+      relativizeNoteDates();
     }
   };
+  
+  
+  // TODO: Leap days?
+  function relativizeNoteDates() {
+
+    var note = $('#note');
+    if (!note.length) return;
+
+    var months = "January February March April May June July August September October November December".split(' ');
+    var shorts = $.map(months, function(m) { return m.substring(0,3) });
+    months = months.concat(shorts).concat(["Sept"]);
+    var months_re = '(?:'+months.join('|')+ ')';
+    var re = new RegExp('(\\b\\d{1,2}\\s+'+months_re+'\\b|\\b'+months_re+'\\s+\\d{1,2}\\b)', 'ig');
+
+    // DEBUG
+    // note.html("July 20, July 19, 21 Jul, Jul 25, Jul 26, Jul 27, Jul 28 and Aug 3, Aug 20.");
+
+    var now = new Date(), today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    note.html(note.html().replace(re, function(_, date) {
+      var distance, target_date = new Date(date + " " + today.getFullYear());
+      if (target_date < today)
+        target_date.setFullYear(target_date.getFullYear() + 1);
+      distance = distanceOfTimeInWords(today, target_date);
+      return date + " ("+distance+")";
+    }));
+
+  }
+  
+  function distanceOfTimeInWords(from, unto) {
+    var days = Math.ceil((unto-from)/1000/24/60/60);
+    if (days == 0)
+      return '<strong class="today">today!</strong>';
+    else if (days == 1)
+      return '<strong class="tomorrow">tomorrow!</strong>';
+    else if (days <= 14)
+      return "in <strong>" + days + " day" + (days==1 ? "" : "s") + "</strong>";
+    else if (days <= 7*3+3)
+      return "in "+Math.round(days/7.0)+" weeks";
+    else if (days <= 31*11+5) {
+      var months = Math.round(days/31.0);
+      return "in " +months+ " month" + (months==1 ? "" : "s");
+    } else
+      return "in a year";
+  }
+
 
   function mendAnchors() {
     var m = location.hash.match(/(__[a-z0-9]{6})$/);
@@ -14,6 +60,7 @@ List = (function() {
     }
   }
   
+
   function initFancyDrilldowns() {
     $(".drilldown").css({position: 'absolute'}).
       parent().mouseover(showDrilldown).mouseout(hideDrilldown);
