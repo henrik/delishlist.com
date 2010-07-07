a0fb8932265b4ee24f74519f28446b302361f7d9
Henrik N <henrik@nyh.se>
2010-07-07 19:36:59 +0200

    Move stylesheets into /stylesheets.

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ get "/" do
   haml :index, :layout => :layout
 end
 
-get "/:stylesheet.css" do
+get "/stylesheets/:stylesheet.css" do
   content_type 'text/css', :charset => 'utf-8'
   sass :"sass/#{params[:stylesheet]}"
 end
