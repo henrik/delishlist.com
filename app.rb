bffb565c17d6177ac8e334c647cefe221120d8f9
Henrik N <henrik@nyh.se>
2010-07-09 19:51:49 +0200

    Extract requires and settings from app.rb.

--- app.rb
+++ app.rb
@@ -1,21 +1,4 @@
-require "rubygems"
-require "sinatra"
-
-require "haml"
-set :haml, :format => :html5, :attr_wrapper => %{"}
-
-cache_minutes = 15  # Since we can't reference settings from each other.
-set :cache_minutes, cache_minutes
-set :cache_seconds, cache_minutes * 60
-
-if ENV['RACK_ENV'] == 'production'
-  set :cache_root, "#{File.dirname(__FILE__)}/../shared/cache"
-else
-  set :cache_root, "/tmp/delishlist"
-end
-
-Dir["{helpers,models,lib}/**/*.rb"].each { |f| require f }
-
+require "#{File.dirname(__FILE__)}/setup.rb"
 
 get "/" do
   haml :index, :layout => :layout
