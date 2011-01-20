051126126420fb8e011b6e8b01328ed8f96be4df
Henrik N <henrik@nyh.se>
2011-01-20 21:28:19 +0100

    User/List models support Pinboard.

--- models/list.rb
+++ models/list.rb
@@ -5,10 +5,11 @@ class List
   attr_reader :user, :description, :items
   
   def initialize(username)
-    delicious = Delicious.new(username, "wishlist")
-    description, items = delicious.tag_description_and_items
-    
     @user = User.new(username)
+
+    parser = @user.parser
+    description, items = parser.tag_description_and_items
+    
     @description = description
     @items = items.map { |hash| Item.new(hash) }.reject { |i| i.exclude? }
   end
--- models/user.rb
+++ models/user.rb
@@ -2,21 +2,32 @@ class User
   attr_reader :name
   
   def initialize(name)
-    @name = name
+    if name.to_s.match(/\Au:(.+)/)
+      @name = $1
+      @pinboard = true
+    else
+      @name = name
+      @pinboard = false
+    end
   end
-    
-  def delicious_url
-    "http://delicious.com/#{name}"
+
+  def pinboard?
+    @pinboard
   end
-  
+
+  def parser
+    klass = pinboard? ? Pinboard : Delicious
+    klass.new(name, "wishlist")
+  end
+
   def possessive
     genitive = name[-1,1] == 's' ? %{’} : %{’s}
     name + genitive
   end
   alias_method :s, :possessive  # "#{@user.s} delishlist"!
-  
+
   def to_s
     name
   end
-  
+
 end
